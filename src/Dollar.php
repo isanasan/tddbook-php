d7402fabcaf69af9ad4739e3f99246053187d248
isanasan <yfcgpsebp@gmail.com>
2021-04-03 05:39:05 +0900

    p.7

---
+++ src/Dollar.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace isanasan\phptddbook;
+
+class Dollar
+{
+    public $amount = 10;
+
+    public function __constractor(){}
+
+    public function times(){}
+}
--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -2,13 +2,11 @@
 
 namespace isanasan\phptddbook;
 
-use function PHPUnit\Framework\assertEquals;
-
 final class MoneyTest extends TestCase
 {
-    public function MoneyTest() {
+    public function testMoney() {
         $five = new Dollar(5);
         $five->times(2);
-        assertEquals(10,$five->amount);
+        $this->assertEquals(10,$five->amount);
     }
 }
