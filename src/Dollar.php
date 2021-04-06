081226760c7814b86c2150c52868a62abd55a433
isanasan <yfcgpsebp@gmail.com>
2021-04-07 05:57:54 +0900

    p.38

--- src/Dollar.php
+++ src/Dollar.php
@@ -2,10 +2,8 @@
 
 namespace isanasan\phptddbook;
 
-class Dollar
+class Dollar extends Money
 {
-    private $amount;
-
     public function __construct(int $amount)
     {
         $this->amount = $amount;
@@ -14,9 +12,5 @@ class Dollar
     public function times(int $multiplier)
     {
         return (new Dollar($this->amount * $multiplier));
-    }
-
-    public function equals(Dollar $object) {
-        return $this->amount === $object->amount;
     }
 }
--- src/Money.php
+++ src/Money.php
@@ -5,4 +5,9 @@ namespace isanasan\phptddbook;
 class Money
 {
     protected $amount;
+
+    public function equals(Money $object): bool
+    {
+        return $this->amount === $object->amount;
+    }
 }
