2d2bfdd807b392df445cd6b3b46366d479bdd068
isanasan <yfcgpsebp@gmail.com>
2021-04-23 13:29:28 +0900

    p.146

--- xunit/src/WasRun.php
+++ xunit/src/WasRun.php
@@ -4,16 +4,18 @@ namespace isanasan\phptddbook;
 
 class WasRun
 {
+    public $wasRun;
     public $name;
 
     public function __construct($name)
     {
         $this->wasRun = 'none';
+        $this->name = $name;
     }
 
     public function run()
     {
-        $this->testMethod();
+        call_user_func([$this, $this->name]);
     }
 
     public function testMethod()
